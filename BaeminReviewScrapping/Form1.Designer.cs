e8047583267d2ed5f0a134495187bd8b114ada50
Zerry Wit <zerry319.wit@gmail.com>
2024-04-22 10:58:53 +0800

    fix bug

--- BaeminReviewScrapping/Form1.Designer.cs
+++ BaeminReviewScrapping/Form1.Designer.cs
@@ -31,11 +31,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lat = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Lon = new System.Windows.Forms.TextBox();
+            this.LocationNum = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(79, 60);
+            this.button1.Location = new System.Drawing.Point(76, 112);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -45,7 +50,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(225, 60);
+            this.button2.Location = new System.Drawing.Point(222, 112);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 1;
@@ -55,18 +60,63 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(12, 21);
+            this.progressBar1.Location = new System.Drawing.Point(12, 69);
             this.progressBar1.Maximum = 10000;
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(352, 23);
             this.progressBar1.Step = 1;
             this.progressBar1.TabIndex = 2;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Latitude";
+            // 
+            // Lat
+            // 
+            this.Lat.Location = new System.Drawing.Point(63, 18);
+            this.Lat.Name = "Lat";
+            this.Lat.Size = new System.Drawing.Size(100, 20);
+            this.Lat.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(203, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Longitude";
+            // 
+            // Lon
+            // 
+            this.Lon.Location = new System.Drawing.Point(264, 18);
+            this.Lon.Name = "Lon";
+            this.Lon.Size = new System.Drawing.Size(100, 20);
+            this.Lon.TabIndex = 6;
+            // 
+            // LocationNum
+            // 
+            this.LocationNum.AutoSize = true;
+            this.LocationNum.Location = new System.Drawing.Point(170, 48);
+            this.LocationNum.Name = "LocationNum";
+            this.LocationNum.Size = new System.Drawing.Size(0, 13);
+            this.LocationNum.TabIndex = 7;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 95);
+            this.ClientSize = new System.Drawing.Size(376, 147);
+            this.Controls.Add(this.LocationNum);
+            this.Controls.Add(this.Lon);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Lat);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -75,6 +125,7 @@
             this.Name = "Form1";
             this.Text = "BaeminReviewScrapping";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +134,11 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Lat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Lon;
+        private System.Windows.Forms.Label LocationNum;
     }
 }
 
